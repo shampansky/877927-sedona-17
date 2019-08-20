f1e0bfed244dcc08a66753f48f33248f5c324a08
shampansky <y.shadrin@gmail.com>
2019-08-20 08:46:48 +0300

    вернул главное меню на мобильной версии в поток

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,14 @@
 'use stirct';
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__menu-btn');
+var mainNavButton = document.querySelector('.main-nav__menu-btn');
 var header = document.querySelector('.page-header');
 header.classList.remove('page-header--nojs');
-if (navToggle) {
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-nav--opened')) {
-      navMain.classList.remove('main-nav--opened');
+if (header) {
+  mainNavButton.addEventListener('click', function() {
+    if (header.classList.contains('page-header--opened')) {
+      header.classList.remove('page-header--opened');
     } else {
-      navMain.classList.add('main-nav--opened');
+      header.classList.add('page-header--opened');
     }
   });
 }
